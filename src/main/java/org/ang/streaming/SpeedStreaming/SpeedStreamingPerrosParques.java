96420032b0c6820709858e7b3ff4f4d17f4e02c6
German Bernal <gd.bernal10@uniandes.edu.co>
2016-04-03 22:02:48 +0000

    Parseo y Persistencia en mongo

--- src/main/java/org/ang/streaming/SpeedStreaming/SpeedStreamingPerrosParques.java
+++ src/main/java/org/ang/streaming/SpeedStreaming/SpeedStreamingPerrosParques.java
@@ -9,7 +9,6 @@ package org.ang.streaming.SpeedStreaming;
  *
  * @author Administrator
  */
-
 import java.util.*;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -19,9 +18,15 @@ import java.util.regex.Pattern;
 import scala.Tuple2;
 
 import com.google.common.collect.Lists;
+import com.mongodb.*;
+import com.mongodb.client.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import kafka.serializer.StringDecoder;
-import kafka.serializer.StringEncoder;
-
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.*;
@@ -29,105 +34,149 @@ import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.kafka.*;
 import org.apache.spark.streaming.Durations;
 
-public class SpeedStreamingPerrosParques{
-  
-  private static final Pattern SPACE = Pattern.compile(" ");
-  
-  public static void main(String[] args) {
-    args = new  String[]{"10.0.1.25:9092","gps"};
-    if (args.length < 2) {
-      System.err.println("Usage: SpeedStreamingPerrosParques <brokers> <topics>\n" +
-          "  <brokers> is a list of one or more Kafka brokers\n" +
-          "  <topics> is a list of one or more kafka topics to consume from\n\n");
-      System.exit(1);
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+import org.bson.Document;
+import static java.util.Arrays.asList;
+
+public class SpeedStreamingPerrosParques {
+
+    private static final Pattern SPACE = Pattern.compile(" ");
+    public static final String LOG_PATH = "/home/vagrant/SpeedStreaming/logSpeedStreaming.txt";
+    //public static final String LOG_PATH = "C:\\Users\\Administrator\\Dropbox\\Andes\\4208_Arquitectuta_NG\\taller5\\MaquinaIoT\\repositorio\\SpeedStreaming\\logSpeedStreaming.txt";
+
+    public static void main(String[] args) throws Exception {
+        //args = new String[]{"10.0.1.25:9092", "gps"};
+        if (args.length < 2) {
+            System.err.println("Usage: SpeedStreamingPerrosParques <brokers> <topics>\n"
+                    + "  <brokers> is a list of one or more Kafka brokers\n"
+                    + "  <topics> is a list of one or more kafka topics to consume from\n\n");
+            System.exit(1);
+        }
+
+        //saveToMongo("{\"id\":\"1\",\"type\":\"collar1\",\"pet_name\":\"Lazzy2\",\"lat\":\"100\",\"log\":\"67\",\"otro\":\"test\"}");
+
+        System.out.println("Checkpoint 1");
+        //StreamingExamples.setStreamingLogLevels();
+
+        String brokers = args[0];
+        String topics = args[1];
+
+        System.out.println("Checkpoint 2");
+
+        // Create context with a 2 seconds batch interval
+        SparkConf sparkConf = new SparkConf().setAppName("SpeedStreamingPerrosParques").setMaster("local[2]");
+        System.out.println("Checkpoint 2.1");
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
+
+        System.out.println("Checkpoint 3");
+
+        HashSet<String> topicsSet = new HashSet<String>(Arrays.asList(topics.split(",")));
+        HashMap<String, String> kafkaParams = new HashMap<String, String>();
+        kafkaParams.put("metadata.broker.list", brokers);
+
+        System.out.println("Checkpoint 4");
+
+        // Create direct kafka stream with brokers and topics
+        JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
+                jssc,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                kafkaParams,
+                topicsSet
+        );
+
+        System.out.println("Checkpoint 5");
+
+        // Get the lines, split them into words, count the words and print
+        JavaDStream<String> lines = messages.map(new org.apache.spark.api.java.function.Function<Tuple2<String, String>, String>() {
+            @Override
+            public String call(Tuple2<String, String> tuple2) throws Exception {
+                toFile("Method: Main: Trace: " + "Line MQ: " + tuple2._2, LOG_PATH);
+                saveToMongo(tuple2._2);
+                return tuple2._2();
+            }
+        });
+        lines.print();
+        jssc.start();
+        jssc.awaitTermination();
     }
-    System.out.println("Checkpoint 1");
-    //StreamingExamples.setStreamingLogLevels();
 
-    String brokers = args[0];
-    String topics = args[1];
-    
-    System.out.println("Checkpoint 2");
-    
-    // Create context with a 2 seconds batch interval
-    //SparkConf sparkConf = new SparkConf().setAppName("SpeedStreamingPerrosParques").setMaster("spark://localhost:18080").set("spark.ui.port","18080");
-    SparkConf sparkConf = new SparkConf().setAppName("SpeedStreamingPerrosParques").setMaster("local[2]");
-    System.out.println("Checkpoint 2.1");
-    JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
-    
-     System.out.println("Checkpoint 3");
-    
-    HashSet<String> topicsSet = new HashSet<String>(Arrays.asList(topics.split(",")));
-    HashMap<String, String> kafkaParams = new HashMap<String, String>();
-    kafkaParams.put("metadata.broker.list", brokers);
-    
-    Map<String,Integer> kafkaParams2 = new HashMap<String, Integer>();
-    kafkaParams2.put(topics, 1);
-       
-    
-    System.out.println("Checkpoint 4");
-    
-    //JavaPairInputDStream<String, String> messages1 = KafkaUtils.createStream(jssc, brokers, "", kafkaParams2);
-    
-    //System.out.println(messages1);
-    
-    StringEncoder a = null;
-    
-    
-    
-    // Create direct kafka stream with brokers and topics
-    
-    JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(
-        jssc,
-        String.class,
-        String.class,
-        StringDecoder.class,
-        StringDecoder.class,
-        kafkaParams,
-        topicsSet
-    );
-
-     System.out.println("Checkpoint 5");
-    
-    // Get the lines, split them into words, count the words and print
-    JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
-      @Override
-      public String call(Tuple2<String, String> tuple2) {
-        return tuple2._2();
-      }
-    });
-    
-     System.out.println("Checkpoint 6");
-    
-    JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-      @Override
-      public Iterable<String> call(String x) {
-        return Lists.newArrayList(SPACE.split(x));
-      }
-    });
-    
-     System.out.println("Checkpoint 7");
-    
-    JavaPairDStream<String, Integer> wordCounts = words.mapToPair(
-      new PairFunction<String, String, Integer>() {
-        @Override
-        public Tuple2<String, Integer> call(String s) {
-          return new Tuple2<String, Integer>(s, 1);
+    public static void toFile(String data, String fileNameFullPath) throws IOException {
+        List<String> lines = Arrays.asList(data);
+        Path file = Paths.get(fileNameFullPath);
+        if (file.toFile().exists()) {
+            Files.write(file, lines, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+        } else {
+            Files.write(file, lines, Charset.forName("UTF-8"));
         }
-      }).reduceByKey(
-        new Function2<Integer, Integer, Integer>() {
-        @Override
-        public Integer call(Integer i1, Integer i2) {
-          return i1 + i2;
-        }
-      });
-    
-     System.out.println("Checkpoint 8");
-    
-    wordCounts.print();
+    }
 
-    // Start the computation
-    jssc.start();
-    jssc.awaitTermination();
-  }
+    private static void saveToMongo(String jsonData) throws Exception {
+        try {
+            toFile("Method: saveToMongo: Trace: " + "** BEGIN EXEC ** ", LOG_PATH);
+            MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://10.1.1.110:27017"));
+            MongoDatabase db = mongoClient.getDatabase("kml_db");
+            toFile("Method: saveToMongo: Trace: " + "db: " + db.getName(), LOG_PATH);
+            toFile("Method: saveToMongo: Trace: " + "db: " + db.listCollections(), LOG_PATH);
+            JSONArray array = JsonDecode(jsonData);
+            toFile("Method: saveToMongo: Trace: " + "JSONArray: " + array.toString(), LOG_PATH);
+            toFile("Method: saveToMongo: Trace: " + "JSONObject: " + array.get(0).toString(), LOG_PATH);
+            JSONObject data = (JSONObject) array.get(0);
+            //JSONObject data = (JSONObject)JsonDecode(jsonData).get(0);
+            toFile("Method: saveToMongo: Trace: " + "Data: " + data.toJSONString(), LOG_PATH);
+            Document key = new Document("id_collar", data.get("id"));
+            toFile("Method: saveToMongo: Trace: " + "key: " + key.toJson(), LOG_PATH);
+            toFile("Method: saveToMongo: Trace: " + "Data Exists: " + db.getCollection("collares_perros").find(key).first(), SpeedStreamingPerrosParques.LOG_PATH);
+            if (db.getCollection("collares_perros").find(key).first() == null) {
+                db.getCollection("collares_perros").insertOne(key
+                        .append("type", data.get("type"))
+                        .append("pet_name", data.get("pet_name"))
+                        .append("lat", data.get("lat"))
+                        .append("log", data.get("log"))
+                        .append("createdAt", System.currentTimeMillis())
+                );
+            } else {
+                db.getCollection("collares_perros").updateOne(
+                        key,
+                        new Document("$set",
+                                new Document("type", data.get("type"))
+                                .append("pet_name", data.get("pet_name"))
+                                .append("lat", data.get("lat"))
+                                .append("log", data.get("log"))
+                                .append("createdAt", System.currentTimeMillis())
+                        )
+                );
+            }
+            toFile("Method: saveToMongo: Trace: " + "** END EXEC ** ", LOG_PATH);
+        } catch (Exception e) {
+            toFile("Method: saveToMongo, Exception: " + e.getMessage(), LOG_PATH);
+        }
+    }
+
+    public static JSONArray JsonDecode(String jsonData) throws IOException {
+        try {
+            toFile("Method: JsonDecode: Trace: " + "** BEGIN EXEC ** ", LOG_PATH);
+            JSONParser parser = new JSONParser();
+            toFile("Method: JsonDecode: Trace: " + "Input:  jsonData: " + jsonData, LOG_PATH);
+            jsonData = jsonData.indexOf("[") == 1 ? jsonData : "[" + jsonData + "]";
+            toFile("Method: JsonDecode: Trace: " + "Input:  jsonData - Final: " + jsonData, LOG_PATH);
+            Object obj = parser.parse(jsonData);
+            JSONArray array = (JSONArray) obj;
+            //JSONObject obj2 = (JSONObject) array.get(1);
+            toFile("Method: JsonDecode: Trace: " + "** END EXEC ** ", LOG_PATH);
+            return array;
+        } catch (Exception pe) {
+
+            //System.out.println("position: " + pe.getPosition());
+            //System.out.println(pe);
+            toFile("Method: JsonDecode, Exception: " + pe.getMessage(), LOG_PATH);
+            return null;
+        }
+    }
 }
